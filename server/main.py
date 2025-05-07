96cd2a2fb6f1b19e75a85cfac9c50bff06cbe86b
ljy2855 <ljy2855@naver.com>
2025-05-07 19:09:39 +0900

    feat: add deploy workflow

--- server/main.py
+++ server/main.py
@@ -3,8 +3,7 @@ from fastapi import FastAPI
 from server.routers.chat import chat_router
 from server.routers.document import document_router
 
-app = FastAPI()
-
+app = FastAPI(root_path="/server")
 
 app.include_router(document_router, prefix="/documents", tags=["documents"])
 app.include_router(chat_router, tags=["chat"])
