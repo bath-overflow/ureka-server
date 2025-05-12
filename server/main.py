ed4fd7bfa5e3504a884f0da9fec4d42421c37cdb
ljy2855 <ljy2855@naver.com>
2025-05-12 22:45:44 +0900

    fix: add create table

--- server/main.py
+++ server/main.py
@@ -1,8 +1,20 @@
+import os
+
 from fastapi import FastAPI
 
+import server.models.document_model
+import server.models.project_model
 from server.routers.chat import chat_router
 from server.routers.document import router as document_router
 from server.routers.project import router as project_router
+from server.utils.db import Base, engine
+
+_ = server.models.project_model
+_ = server.models.document_model
+
+if os.environ.get("ENVIRONMENT") != "test":
+    Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
